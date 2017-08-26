90485d6a3b7e43b282189b74813c7af03991b151
strongway <shizhuanghua@gmail.com>
2017-08-26 23:23:35 +0200

    solved space and question mark issue for audio download. And post_ulr issue ( https instead of http).

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+#%%
 import requests, tempfile, sys
 from variables import cookies
 from multiprocessing import Pool
 from lxml import html
 from lxml.etree import tostring
-
+#%%
 def upload_file_to_server(thing_id, cell_id, course, file):
 	files = {'f': ('whatever.mp3', open(file.name, 'rb'), 'audio/mp3')}
 	form_data = { 
@@ -14,16 +15,16 @@ def upload_file_to_server(thing_id, cell_id, course, file):
 	headers = {
 		"User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:35.0) Gecko/20100101 Firefox/35.0",
 		"referer": course}
-	post_url = "http://www.memrise.com/ajax/thing/cell/upload_file/"
+	post_url = "https://www.memrise.com/ajax/thing/cell/upload_file/"
 	r = requests.post(post_url, files=files, cookies=cookies, headers=headers, data=form_data, timeout=60)
-
+#%%
 def get_audio_files_from_course(first_database_page, number_of_pages):
 	database_urls = []
 	for page in range(1, number_of_pages + 1):
 		database_urls.append(first_database_page + '?page=' + str(page))
 	pool = Pool(processes = 7)
 	pool.map(get_thing_information, database_urls)
-
+#%%
 def get_thing_information(database_url):
 	print(database_url)
 	response = requests.get(database_url, cookies = cookies)
@@ -33,16 +34,16 @@ def get_thing_information(database_url):
 	for div in div_elements:
 		thing_id = div.attrib['data-thing-id']
 		try:
-			chinese_word = div.xpath("td[2]/div/div/text()")[0]
+			german_word = div.xpath("td[2]/div/div/text()")[0]
 		except IndexError:
 			print("failed to get the word of item with id " + str(thing_id) + ' on ' + str(database_url))
 			continue
 		column_number_of_audio = div.xpath("td[contains(@class, 'audio')]/@data-key")[0]
 		audio_files = div.xpath("td[contains(@class, 'audio')]/div/div[contains(@class, 'dropdown-menu')]/div")
 		number_of_audio_files = len(audio_files)
-		audios.append({'thing_id': thing_id, 'number_of_audio_files': number_of_audio_files, 'chinese_word': chinese_word, 'column_number_of_audio': column_number_of_audio})
+		audios.append({'thing_id': thing_id, 'number_of_audio_files': number_of_audio_files, 'german_word': german_word, 'column_number_of_audio': column_number_of_audio})
 	sequence_through_audios(audios)
-
+#%%
 def download_audio(path):
 	response = requests.get(path)
 	if len(response.content) < 100:
@@ -54,23 +55,24 @@ def download_audio(path):
 		return temp_file
 	else:
 		return 'Bad response when downloading file'
-
+#%%
 def sequence_through_audios(audios):
 	for audio in audios:
 		if audio['number_of_audio_files'] > 0:
 			continue
 		else:
-			requests.post('http://soundoftext.com/sounds', data={'text':audio['chinese_word'], 'lang':'zh-CN'}) # warn the server of what file I'm going to need
-			temp_file = download_audio('http://soundoftext.com/static/sounds/zh-CN/' + audio['chinese_word'] + '.mp3') #download audio file
+			requests.post('http://soundoftext.com/sounds', data={'text':audio['german_word'].replace('?',''), 'lang':'de'}) # warn the server of what file I'm going to need
+			#german_word = german_word.replace(" ","_")
+			temp_file = download_audio('http://soundoftext.com/static/sounds/de/' + audio['german_word'].replace(' ','_').replace('?','') + '.mp3') #download audio file
 			if isinstance(temp_file, str):
-				print(audio['chinese_word'] + ' skipped: ' + temp_file)
+				print(audio['german_word'] + ' skipped: ' + temp_file)
 				continue
 			else:
 				upload_file_to_server(audio['thing_id'], audio['column_number_of_audio'], course_database_url, temp_file)
 				temp_file.close()
-				print(audio['chinese_word'] + ' succeeded')
+				print(audio['german_word'] + ' succeeded')
 
-
+#%%
 if __name__ == "__main__":
 	course_database_url = sys.argv[1]
 	number_of_pages = int(html.fromstring(requests.get(course_database_url, cookies=cookies).content).xpath("//div[contains(@class, 'pagination')]/ul/li")[-2].text_content())
